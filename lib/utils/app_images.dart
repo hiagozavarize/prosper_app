72872d981170f0daeaa6f9205d458f5ca8ad1e3b
Hiago Zavarize <hiagozavarizex2@gmail.com>
2024-07-01 22:15:55 -0300

    feat: create welcome screen widget and adding poppins font

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,3 @@
+abstract class AppImages {
+  static const String logoProsper = "lib/assets/logo_prosper.png";
+}
---
+++ lib/welcome.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'utils/utils.dart';
+
+class Welcome extends StatelessWidget {
+  static const String screenName = "/";
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(35),
+        child: SizedBox.expand(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(height: 24),
+              Image.asset(
+                AppImages.logoProsper,
+                width: 102,
+                height: 158,
+              ),
+              const SizedBox(height: 80),
+              const Text(
+                'Bem Vindo ao ',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 45,
+                    fontWeight: FontWeight.w800),
+              ),
+              Text(
+                'Prosper',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: AppColors.primaryColor,
+                  fontSize: 50,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              const SizedBox(height: 200),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                ),
+                child: Text(
+                  'Continuar',
+                  style: GoogleFonts.poppins(
+                    textStyle: const TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
