bb622597e1b9719c1d9cd0f11fb67262142c9d00
Hiago <78562407+hiagozavarizex2@users.noreply.github.com>
2024-08-03 22:21:08 -0300

    no changes

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColors {
-  static Color get primaryColor => const Color.fromARGB(255, 87, 174, 0);
+  static Color get primaryColor =>const Color.fromARGB(255, 87, 174, 0);
   static Color get scaffoldBackGroundColor =>
       const Color.fromARGB(255, 0, 0, 0);
 }
